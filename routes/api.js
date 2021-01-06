a1e883383e33239c1cedbc879f9acd9712d8418b
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-06 09:33:51 -0600

    updating issues for heroku

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const workout = require("../models/Workout.js");
+const workout = require("../models/workout.js");
 
 router.post("/api/workout", ({ body }, res) => {
   workout.create(body)
