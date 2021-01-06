6b3c11acc49a8e0e5728490bd0dafe9393b4f96a
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-06 15:27:11 -0600

    seeds update

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,7 +4,7 @@ let db = require("../models");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
-// mongoose.set("useUnifiedTopology", true);
+mongoose.set("useUnifiedTopology", true);
 
 let workoutSeed = [
   {
