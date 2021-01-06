40fbc75541027035ce0087089e03c0419c09251d
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-06 15:49:17 -0600

    mongoose database update

--- seeders/seed.js
+++ seeders/seed.js
@@ -5,7 +5,7 @@ let db = require("../models");
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // });
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/WorkoutDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
