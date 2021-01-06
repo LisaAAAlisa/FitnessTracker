3bf44d874b6983e0d23d8d00eda4a2d9c0b04b12
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-06 15:44:16 -0600

    seed update re mongo connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,13 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 let workoutSeed = [
